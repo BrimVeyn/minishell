5d56689a105229e487d18ecb23dd94339e9752f7
Bryan Van paemel <bvan-pae@made-f0Ar3s7.clusters.42paris.fr>
2023-12-26 16:35:02 +0100

    instant test

--- source/exec_pipe.c
+++ source/exec_pipe.c
@@ -3,45 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   exec_pipe.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nbardavi <nbabardavid@gmail.com>           +#+  +:+       +#+        */
+/*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 15:06:31 by nbardavi          #+#    #+#             */
-/*   Updated: 2023/12/26 15:17:57 by nbardavi         ###   ########.fr       */
+/*   Updated: 2023/12/26 16:34:31 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	exec_pipe(t_data *data, char *env[])
+void exec_pipe()
 {
-	if (pipe(data->pipefd) == -1)
-		exit(EXIT_FAILURE);
-	data->pid[data->cptfork] = fork();
-	if (data->pid[data->cptfork] != 0)
+
+}
+
+void set_output()
+{
+
+}
+
+void set_input(t_args *arg)
+{
+	if (args->trigger = S_AL)
 	{
-		close(data->pipefd[1]);
-		dup2(data->pipefd[0], STDIN_FILENO);
-		close(data->pipefd[0]);
+		dup2(); //INPUT
 	}
-	if (data->pid[data->cptfork] == 0)
+	if (args->trigger = D_AL)
 	{
-		close(data->pipefd[0]);
-		close(data->input);
-		if (data->cptfork != data->maxcmd - 1)
-			dup2(data->pipefd[1], STDOUT_FILENO);
-		else
-			dup2(data->output, STDOUT_FILENO);
-		close(data->pipefd[1]);
-		close(data->output);
-		execve(data->cmds[data->cptfork][0], data->cmds[data->cptfork], env);
-		exit(EXIT_FAILURE);
+		heredoc();
 	}
 }
 
-void ms_while_pipe(t_args **args)
+void ms_main_pipe(t_args **args)
 {
 	int i;
+	
+	set_input(args[0]);
+	while()
+	{
 
-	i = 0;
-	while (args[i])
+	}
+	set_output();
 }
--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:16:24 by bvan-pae          #+#    #+#             */
-/*   Updated: 2023/12/26 15:21:44 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2023/12/26 16:25:06 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ void free_env(t_env *env)
 
 int main(int ac, char *av[], char *env[])
 {
-	t_env data;
+	t_env denv;
 	
 	(void) av;
 	if (!env || ac >= 2)
 		perror("Env is null");
-	data.f_env = ms_dupdup(env);
-	data.pwd = get_pwd(&data);
-	data.path = get_path(&data);
-	data.usr = get_usr(&data);
-	// printf("%s\n", data.pwd);
-	// printf("%s\n", data.path);
-	// printf("%s\n", data.usr);
+	denv.f_env = ms_dupdup(env);
+	denv.pwd = get_pwd(&denv);
+	denv.path = get_path(&denv);
+	denv.usr = get_usr(&denv);
+	// printf("%s\n", denv.pwd);
+	// printf("%s\n", denv.path);
+	// printf("%s\n", denv.usr);
 	char *c_line = "\"";
 	// for (int i = 0; c_line[i]; i++)
 	// 	printf("%d\n", c_line[i]);
-	prompt(&data);
-	free_env(&data);
+	prompt(&denv);
+	free_env(&denv);
 }
