7ca814266ce8c2a24830923ad4c9c798081b5195
Bryan Van paemel <bvan-pae@made-f0Ar3s10.clusters.42paris.fr>
2024-01-10 15:17:47 +0100

    string done, errno and more

--- source/path_parse.c
+++ source/path_parse.c
@@ -6,12 +6,30 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 09:31:25 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/10 10:22:08 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/01/10 15:07:34 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include <unistd.h>
+#include <sys/stat.h>
+
+int	filetype(char *path)	
+{
+	struct stat path_stat;
+
+	stat(path, &path_stat);
+	if (S_ISREG(path_stat.st_mode)) 
+	{
+		return (FAILE);
+		// printf("%s est un fichier régulier.\n", path);
+	} 
+	else 
+	{
+		return(ERROR);
+		// printf("%s est un type de fichier non pris en charge.\n", path);
+	}
+}
 
 char	*join_path(char *cmd, t_env *denv)
 {
@@ -19,13 +37,20 @@ char	*join_path(char *cmd, t_env *denv)
 	char **paths;
 	char *cmd_cpy;
 
-	paths = ft_split(denv->path, ':');
 	i = 0;
+	// printf("This is the cmd : %s\n", cmd);
+	if (!access(cmd, X_OK) && filetype(cmd) == FAILE)
+		return (cmd);
+	paths = ft_split(denv->path, ':');
+	if (!ft_strncmp(cmd, "", 2))
+    {
+		fd_printf(2, "'': command not found\n");
+		return (NULL);
+    }
 	while (paths[i])
 	{
 		cmd_cpy = ft_strjoin_s2(paths[i], ft_strjoin_s2("/", ft_strdup(cmd)));
-		// printf("%s\n", cmd_cpy);
-		if (!access(cmd_cpy, X_OK | F_OK))
+		if (!access(cmd_cpy, X_OK) && filetype(cmd_cpy) == FAILE)
 		{
 			free_tab(paths);
 			free(cmd);
@@ -35,18 +60,27 @@ char	*join_path(char *cmd, t_env *denv)
 		i++;
 	}
 	free_tab(paths);
-	return(cmd);
+	return(NULL);
 }
 
 void	ms_add_path(t_tok *tdata, t_env *denv)
 {
 	int i;
+	char *save;
 
 	i = 0;
 	while (tdata->tokens[i])
 	{
 		if (tdata->type[i] == CMD)
+        {
+			save = tdata->tokens[i][0];
 			tdata->tokens[i][0] = join_path(tdata->tokens[i][0], denv);
+			if (tdata->tokens[i][0] == NULL)
+			{
+				fd_printf(2, "%fs: command not found\n", save);
+				tdata->type[i] = WRONG;
+			}
+        }
 		i++;
 	}
 }
