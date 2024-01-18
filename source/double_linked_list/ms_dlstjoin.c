fc9347e13a49c6d4c7739ed318fa0c7bac2c84d7
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2024-01-18 14:17:25 +0100

    norminette...

--- source/double_linked_list/ms_dlstjoin.c
+++ source/double_linked_list/ms_dlstjoin.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_starjoin.c                                      :+:      :+:    :+:   */
+/*   ms_dlstjoin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 12:26:05 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/18 11:38:47 by bvan-pae         ###   ########.fr       */
+/*   Created: 2024/01/18 11:32:04 by bvan-pae          #+#    #+#             */
+/*   Updated: 2024/01/18 11:38:43 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-char *ms_starjoin(t_starlist **slist)
+char *ms_dlstjoin(t_dlist **dlist)
 {
-    if (!slist || !*slist) return NULL;
+    if (!dlist || !*dlist) return NULL;
 
-    t_starlist *current = *slist;
+    t_dlist *current = *dlist;
     char *new = ft_strdup("");
     char *temp;
 
@@ -24,6 +24,11 @@ char *ms_starjoin(t_starlist **slist)
         temp = ft_strjoin(new, current->str);
         free(new);
         new = temp;
+        if (current->next != NULL) {
+            temp = ft_strjoin(new, " ");
+            free(new);
+            new = temp;
+        }
         current = current->next;
     }
     return new;
--- source/misc/ms_setint.c
+++ source/misc/ms_setint.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_dlstclear.c                                     :+:      :+:    :+:   */
+/*   ms_setint.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 11:07:10 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/18 10:51:22 by bvan-pae         ###   ########.fr       */
+/*   Created: 2024/01/18 12:27:10 by bvan-pae          #+#    #+#             */
+/*   Updated: 2024/01/18 12:27:28 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ms_dlstclear(t_dlist **head)
+void ms_setint(int *i, int value)
 {
-	while (*head)
-	{
-		free((*head)->str);
-		ms_dlstdelone(head);
-	}
+	*i = value;
 }
