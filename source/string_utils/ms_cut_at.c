22b55cefb5edab11bab67abfd06d7eea425c5429
Bryan Van paemel <bvan-pae@made-f0Ar3s10.clusters.42paris.fr>
2024-01-23 08:47:43 +0100

    heredoc ok

--- source/string_utils/ms_cut_at.c
+++ source/string_utils/ms_cut_at.c
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:50:40 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/22 14:50:59 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/01/23 08:38:45 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ char *ms_cut_at(char *input, char c)
 	i = 0;
 	while (input[i] && input[i] != c)
 		i++;
+	ft_printf("i = %d\n", i);
+	if (i == 0)
+		return (free(input), ft_strdup(""));
 	new = ft_substr(input, 0, i);
 	free(input);
 	return (new);
