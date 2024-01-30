df585b468c789082e8b8fa7ab4afc8bedb4491ad
Bryan Van paemel <bvan-pae@paul-f3Br5s6.clusters.42paris.fr>
2024-01-30 15:28:12 +0100

    last merge

--- source/parsing/ft_splitm.c
+++ source/parsing/ft_splitm.c
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:23:00 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/30 12:29:36 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:00:57 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,14 @@ static int	count_words(char *str)
 	while (str[x[IX]])
 	{
 		x[TRIGGER] = 0;
-		while ((str[x[IX]] && !ms_isws(str[x[IX]])) || (str[x[IX]] && (x[DQ]
-					|| x[SQ])))
+		while ((str[x[IX]] && !ms_isws(str[x[IX]])) || (str[x[IX]] && (x[DQ] || x[SQ])))
 		{
 			x[DQ] ^= (str[x[IX]] == DQUOTE);
 			x[SQ] ^= (str[x[IX]] == SQUOTE);
 			x[TRIGGER] = 1;
 			x[IX]++;
 		}
-		if (x[TRIGGER] && x[DQ] == 0 && x[SQ] == 0)
+		if (x[TRIGGER])
 			x[COUNT]++;
 		while (str[x[IX]] && ms_isws(str[x[IX]]))
 			x[IX]++;
@@ -50,16 +49,16 @@ void	fill_split(char **split, char *str)
 	while (ms_setint(&x[K], 0), str[x[I]])
 	{
 		x[5] = 0;
-		while ((str[x[I]] && !ms_isws(str[x[I]])) || (str[x[I]] && (x[3]
-					|| x[4])))
+		while ((str[x[I]] && !ms_isws(str[x[I]])) || (str[x[I]] && (x[3] || x[4])))
 		{
+			// ft_printf("STR[%d] = |%c|\n", x[I], str[x[I]]);
 			x[3] ^= (str[x[I]] == DQUOTE);
 			x[4] ^= (str[x[I]] == SQUOTE);
 			x[5] = 1;
 			x[K]++;
 			x[I]++;
 		}
-		if (x[5] == 1 && !(x[3]) && !(x[4]))
+		if (x[5] == 1)
 		{
 			split[x[J]] = ft_substr(str, x[I] - x[K], x[K]);
 			x[J]++;
@@ -72,30 +71,63 @@ void	fill_split(char **split, char *str)
 int	check_dollar(char **split, t_env *denv)
 {
 	char *tmp;
-	char *a_tmp;
 	
 	tmp = NULL;
-	a_tmp = NULL;
 	if (split[0] && split[0][0] == '$')
 		tmp = r_env(split[0], denv);
 	if (tmp && ft_strlen(tmp) == 0)
-		return (free(tmp), free(a_tmp), ERROR);
-	return (free(tmp), free(a_tmp), TRUE);
+		return (free(tmp), ERROR);
+	return (TRUE);
 }
 
 void	transform_split(char **split, t_env *denv)
 {
 	t_starlist	*strl;
 	int			x[2];
+	int			flag;
 
 	x[I] = 0;
+	flag = 0;
 	strl = NULL;
+	// ft_printf("LOL");
+	// for (int i = 0; split[i]; i++)
+		// ft_printf("SPLIT [%d] = %fs\n", i, split[i]);
 	if (check_dollar(split, denv) == ERROR)
 	{
+		flag = 1;
+		// ft_printf("FDP");
+		// int i = 1;
+		// if (split[i])
+		// {
+		// 	while (split[i])
+		// 	{
+		// 		split[i - 1] = split[i];
+		// 		i++;
+		// 	}
+		// 	split[i - 1] = NULL;
+		// 	// for(int j = 0; split[j]; j++)
+		// 	// 	ft_printf("split[%d] = %fs\n", j, split[j]);
+		// 	return ;
+		// }
 		free(split[x[0]]);
 		split[x[0]] = ft_strdup_char(-19);
-		return ;
+		if (split[1] == NULL)
+        {
+			return ;
+        }
 	}
+	int i = 1;
+	if (split[i] && flag == 1)
+	{
+		while (split[i])
+		{
+			split[i - 1] = split[i];
+			i++;
+		}
+		split[i - 1] = NULL;
+	}
+	// for(int i = 0; split[i]; i++)
+	// 	ft_printf("split[i] = %fs\n", split[i]);
 	while (ms_setint(&x[J], ZERO), split[x[I]])
 	{
 		split[x[I]] = tild_expand(split[x[I]], denv);
@@ -129,6 +161,7 @@ char	**ft_splitm(char *str, t_env *denv)
 	{
 		return (dupdup());
 	}
+	// ft_printf("STR = %fs\n", str);
 	fill_split(split, str);
 	transform_split(split, denv);
 	return (split);
