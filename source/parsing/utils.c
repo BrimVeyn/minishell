885c8db36b993838629e35dab7d20fef05f621fc
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2024-01-31 09:50:58 +0100

    quotes

--- source/parsing/utils.c
+++ source/parsing/utils.c
@@ -6,31 +6,42 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:51:50 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/01/26 09:28:58 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/01/31 09:29:47 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+char	*return_delimiter(t_tok *tdata, t_tokh *v, int *q)
+{
+	if (q[0] == 0)
+		return (ms_strtrimf(tdata->tokens[v->j][0], "\""));
+	else if (q[1] == 0)
+		return (ms_strtrimf(tdata->tokens[v->j][0], "\'"));
+	else
+		return (tdata->tokens[v->j][0]);
+}
+
 void	extract_delimiter(char *input, t_tok *tdata, t_tokh *v)
 {
 	int		len;
-	char	*tmp;
+	int		q[2];
 
 	len = 0;
+	q[0] = 0;
+	q[1] = 1;
 	while (input[v->i] && ms_isws(input[v->i]))
 		v->i++;
-	while (input[v->i] && !ms_isws(input[v->i])
-		&& ms_tiktok(&input[v->i]).type == CMD)
+	while (input[v->i] && ((!ms_isws(input[v->i]) && ms_tiktok(&input[v->i]).type == CMD) || (q[0] || q[1])))
 	{
+		q[0] ^= (input[v->i] == '\"');
+		q[1] ^= (input[v->i] == '\'');
 		len++;
 		v->i++;
 	}
 	tdata->tokens[v->j] = (char **)ft_calloc(2, sizeof(char *));
 	tdata->tokens[v->j][0] = ft_substr(input, v->i - len, len);
-	tmp = ms_strtrimf(tdata->tokens[v->j][0], "'");
-	tmp = ms_strtrimf(tmp, "\"");
-	tdata->tokens[v->j][0] = tmp;
+	tdata->tokens[v->j][0] = return_delimiter(tdata, v, q);
 	tdata->type[v->j] = DELIMITER;
 	v->j++;
 }
@@ -38,17 +49,26 @@ void	extract_delimiter(char *input, t_tok *tdata, t_tokh *v)
 char	*grep_word(char *input, t_tokh *v)
 {
 	int	len;
+	int	q[2];
 
+	q[0] = 0;
+	q[1] = 0;
 	len = 0;
 	while (input[v->i] && ms_isws(input[v->i]))
 		v->i++;
-	while (input[v->i] && !ms_isws(input[v->i])
-		&& ms_tiktok(&input[v->i]).type == CMD)
+	while (input[v->i] && ((!ms_isws(input[v->i]) && ms_tiktok(&input[v->i]).type == CMD) || (q[0] || q[1])))
 	{
+		q[0] ^= (input[v->i] == '\"');
+		q[1] ^= (input[v->i] == '\'');
 		len++;
 		v->i++;
 	}
-	return (ft_substr(input, v->i - len, len));
+	if (q[0] == 0)
+		return (ms_strtrimf(ft_substr(input, v->i - len, len), "\""));
+	else if (q[1] == 0)
+		return (ms_strtrimf(ft_substr(input, v->i - len, len), "\'"));
+	else
+		return (ft_substr(input, v->i - len, len));
 }
 
 int	ms_wl2(char *ptr)
