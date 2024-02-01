d0df8e1d86903c2f95fd022d604888379dd17bfd
Nathan Bardavid <nbardavi@paul-f3Br6s5.clusters.42paris.fr>
2024-02-01 16:15:54 +0100

    fini toc robin

--- source/prompt2.c
+++ source/prompt2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
+/*   By: nbardavi <nbabardavid@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:47:51 by nbardavi          #+#    #+#             */
-/*   Updated: 2024/02/01 15:06:18 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/02/01 15:38:16 by nbardavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,25 @@ void	init_prompt(t_env **denv, char **input)
 	prompt = ms_form_prompt(*denv);
 	*input = readline(prompt);
 	free(prompt);
+}
+
+void print_t(char ***str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while(str[i])
+	{
+		while(str[i][j])
+		{
+			printf("tok[%d][%d] %s\n", i, j, str[i][j]);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
 }
 
 void	prompt(t_env *denv, int i)
@@ -45,6 +64,7 @@ void	prompt(t_env *denv, int i)
 				ms_lst_b(&denv->history, ms_lst_new(ft_strdup(input)));
 			add_history(input);
 			d_token = parse_input(input, denv);
+			print_t(d_token.tokens);
 			if (ms_main_pipe(d_token, denv) == 1)
 				break ;
 			if (d_token.t_size != ERROR)
