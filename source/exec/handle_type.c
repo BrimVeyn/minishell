2c169e672f3f124f43ec1b820568501ed1510c12
nbardavid <nbabardavid@gmail.com>
2024-02-09 13:02:04 +0100

    valgrind

--- source/exec/handle_type.c
+++ source/exec/handle_type.c
@@ -6,7 +6,7 @@
 /*   By: nbardavi <nbabardavid@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 10:11:19 by nbardavi          #+#    #+#             */
-/*   Updated: 2024/02/09 12:27:36 by nbardavi         ###   ########.fr       */
+/*   Updated: 2024/02/09 13:00:52 by nbardavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,25 @@ void	handle_wrong(t_pipe *d_pipe)
 
 void	handle_and(t_pipe *d_pipe)
 {
-	d_pipe->skip_or = 0;
+	if (d_pipe->p_nbr == 0)
+		d_pipe->skip_or = 0;
 	if (d_pipe->failed == 1 || d_pipe->p_return == 0)
 	{
-		d_pipe->p_return = -1;
 		d_pipe->skip_and = 1;
 		d_pipe->failed = 0;
 	}
+	d_pipe->p_return = -1;
 }
 
 void	handle_or(t_pipe *d_pipe)
 {
+	d_pipe->skip_and = 0;
+	if (d_pipe->p_nbr == 0)
+	{
+		d_pipe->p_return = -1;
+	}
 	if (d_pipe->failed == 0 || d_pipe->p_return == 1)
+	{
 		d_pipe->skip_or = 1;
+	}
 }
