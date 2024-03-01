4297117a204233a9ef9027f8e7e50c6fb08fa49b
Bryan Van paemel <bvan-pae@bess-f1r3s11.clusters.42paris.fr>
2024-03-01 16:32:14 +0100

    parsing ok

--- source/parsing/utils.c
+++ source/parsing/utils.c
@@ -6,12 +6,11 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:51:05 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/02/26 10:00:28 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/03/01 16:01:22 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
 
 
 char	***ms_copy_tok(char ***tokens, int t_size)
